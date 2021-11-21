7082d983a2502109cbd5f32530209b5ef30434da
TedEngineer <tedvu184@gmail.com>
2021-11-21 21:24:29 +1100

    Converting normal function to arrow function

--- arrowFunction.js
+++ arrowFunction.js
@@ -7,9 +7,15 @@ const sum2 = (a, b) => {
   return a + b;
 };
 
+// converting to arrow function - make it more concise
+const sum2 = (a, b) => a + b;
+
 function isPositive(number) {
   return number >= 0;
 }
+
+// converting to arrow function - make it super concise
+const isPositive2 = (number) => number >= 0;
 
 function randomNumber() {
   return Math.random();
